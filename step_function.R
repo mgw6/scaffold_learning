14803cc1a2236ab802453ea732af3a7d2fb17f12
MacGregor <magwin2012@gmail.com>
2021-09-10 13:23:15 -0400

    Added next_step function

--- step_function.R
+++ step_function.R
@@ -13,19 +13,23 @@ vector1 = c(1,2,3)
 vector1
 "
 
-next_L <- function(l_n, h_n, r_l, d)
+
+next_l <- function(l_n, h_n, r_l, d)
 {
-  return(l_n + (r_l*l_n)(1 - l_n/h_n)(d - h_n/l_n)    )
-  
+  return(l_n + (r_l*l_n)(1 - l_n/h_n)(d - h_n/l_n))
 }
 
-next_H <- function(l_n, h_n, r_h, p_n)
+next_h <- function(l_n, h_n, r_h, p_n)
 {
-  
-  
+  return(h_n + (l_n - p_n)*(r_h)*(1-h_n))
 }
 
-next_step <- function(x)
+next_step <- function(l_n, h_n, r_l, d, r_h, p_n)
 {
-  
+  next_step = c(size = 2)
+  next_step[1] = next_l(l_n, h_n, r_l, d)
+  next_step[2] = next_h(l_n, h_n, r_h, p_n)
+  return (next_step)
 }
+
+
