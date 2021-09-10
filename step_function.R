cb953e6563c65614a0fdb81469f0800c20180bf3
MacGregor <magwin2012@gmail.com>
2021-09-10 13:15:31 -0400

    Initial Commit

---
+++ step_function.R
@@ -0,0 +1,31 @@
+
+"
+test_sqr <- function(x)
+{
+  jack = vector(double, 2)
+  #[]
+  return(jack)
+}
+
+test_sqr(5)
+
+vector1 = c(1,2,3)
+vector1
+"
+
+next_L <- function(l_n, h_n, r_l, d)
+{
+  return(l_n + (r_l*l_n)(1 - l_n/h_n)(d - h_n/l_n)    )
+  
+}
+
+next_H <- function(l_n, h_n, r_h, p_n)
+{
+  
+  
+}
+
+next_step <- function(x)
+{
+  
+}
