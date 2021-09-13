b6310cf140f824f774bd29eb2a4a43775d0f176d
MacGregor <magwin2012@gmail.com>
2021-09-13 11:50:27 -0400

    Fixed next_h, updated get_traj

--- step_function.R
+++ step_function.R
@@ -6,12 +6,12 @@ next_l <- function(l_n, h_n, r_l, d)
 
 next_h <- function(l_n, h_n, r_h, p_n)
 {
-  return(h_n + (l_n - p_n)*(r_h)*(1-h_n))
+  return(h_n + (l_n - p_n)*(r_h*h_n)*(1-h_n))
 }
 
 next_step <- function(l_n, h_n, r_l, d, r_h, p_n)
 {
-  #This function may  be more work than its worth. 
+  #This function is more work than its worth. 
   next_step = c(size = 2)
   next_step[1] = next_l(l_n, h_n, r_l, d)
   next_step[2] = next_h(l_n, h_n, r_h, p_n)
@@ -21,10 +21,10 @@ next_step <- function(l_n, h_n, r_l, d, r_h, p_n)
 calc_trajectory <- function(l_0, h_0, r_l, r_h, d, num_steps=1000)
 {
   
-  if (num_steps <4) stop("Num_steps must be >=4.")
+  if (num_steps <3) stop("Num_steps must be >=3.")
   
-  l_traj = c(size = 1000)
-  h_traj = c(size = 1000)
+  l_traj = c(size = num_steps)
+  h_traj = c(size = num_steps)
   l_traj[1] = l_0
   h_traj[1] = h_0
   
@@ -34,20 +34,19 @@ calc_trajectory <- function(l_0, h_0, r_l, r_h, d, num_steps=1000)
   for (n in 3: num_steps)
   {
     l_traj[n] = next_l(l_traj[n-1], h_traj[n-1], r_l, d)
-    next_h(l_traj[n-1], h_traj[n-1], r_h, l_traj[n-2] )
+    h_traj[n] = next_h(l_traj[n-1], h_traj[n-1], r_h, l_traj[n-2] )
   }
   
   print(l_traj)
   print(h_traj)
-  return (array(C(l_traj, h_traj), dim = (c(2, num_steps))))
+  #Dear Python, please come back 
+  return (matrix(c(l_traj, h_traj),  nrow = num_steps, ncol = 2, byrow = FALSE))
 }
 
-
-jack = calc_trajectory(.01,.1,.05,.05,.5,4)
-jack
-length(jack)
-
-
+#Ask Dr. Green about discrepancies between R and Excel
+test_result = calc_trajectory(.01,.1,.05,.05,.5,5)
+print(test_result)
+print(test_result[, 2])
 
 
 
