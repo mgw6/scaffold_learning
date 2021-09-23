99f96f3724a0d750c437b85e6f5d60a24c729a54
MacGregor <magwin2012@gmail.com>
2021-09-22 23:51:03 -0400

    Cleaned up file

--- driver.R
+++ driver.R
@@ -1,7 +1,22 @@
 #This is a driver file that will pull functions from other files
-source("step_function.R")
+source("functions.R")
+library(ggplot2)
 
-test_result = calc_trajectory(.01, .015, .05, .05, 2, num_steps = 5)
-print(test_result)
+single_test = calc_trajectory(.01, .015, .05, .05, 2, num_steps = 5)
+print(single_test)
+plot(single_test[,2], single_test[,3])
 
-plot(test_result[,1], test_result[,2])
+multi_test = many_trajectories(seq(0,1,by=.1), .01, .05, .05, 2, l_n1 = NULL, n_steps=10)
+multi_test = many_trajectories(.01, seq(0,1,by=.1), .05, .05, 2, l_n1 = NULL, n_steps=10)
+multi_test = many_trajectories(.01,  .05, seq(0,1,by=.1), .05, 2, l_n1 = NULL, n_steps=10)
+multi_test = many_trajectories(.01,  .05,  .05, seq(0,1,by=.1), 2, l_n1 = NULL, n_steps=10)
+multi_test = many_trajectories(.01,  .05,  .05,  2, seq(0,1,by=.1), l_n1 = NULL, n_steps=10)
+multi_test = many_trajectories(.01,  .05,  .05,  2, 2, l_n1 = seq(0,1,by=.1), n_steps=10)
+
+dim(multi_test)
+
+
+
+
+
+
