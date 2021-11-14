2c3ceaf567ec7bcf3f2115b629b417dfdcd935c0
MacGregor <magwin2012@gmail.com>
2021-11-14 17:08:54 -0500

    Changed color of points that go to (1.5, 1.5) to green

--- Py_Code/graph_np.py
+++ Py_Code/graph_np.py
@@ -41,33 +41,60 @@ h_span = np.arange(h_start, h_end+h_step, h_step)
 print("Entering for-loops")
 for l in range(len_l):
     for h in range(len_h):
+
         
         if not np.isfinite(many_traj[l, h, n_steps, 0]) or many_traj[l, h, n_steps,0] > 100:
             #data.append([l_span[l], h_span[h], 'red'])
             #https://numpy.org/doc/stable/reference/generated/numpy.isfinite.html
             pass
         
+        
+        elif abs(many_traj[l, h, n_steps, 0] - eps) < .1 and abs(many_traj[l, h, n_steps, 1] - 1.5) < eps:
+            data.append([l_span[l], h_span[h], 'green'])
+        
+        
         elif np.abs(many_traj[l, h, n_steps, 0] - many_traj[l, h, n_steps, 1]) < eps:
             data.append([l_span[l], h_span[h], 'blue'])
         
-        elif np.abs(many_traj[l, h, n_steps, 1] - d*(many_traj[l, h, n_steps, 0])) < eps:
-            data.append([l_span[l], h_span[h], 'green'])
-        
         else:
             data.append([l_span[l], h_span[h], 'black'])
+            
+            
+        """
+        elif np.abs(many_traj[l, h, n_steps, 1] - d*(many_traj[l, h, n_steps, 0])) < eps:
+            data.append([l_span[l], h_span[h], 'green'])
+        """
 print("Exited for-loops")
 
 df = pd.DataFrame(data, columns = ["Initial l", "Initial h", "Color"])
 df.plot.scatter(x = "Initial l", y = "Initial h", c = "Color", s=1)
 
-m1 = 1.5
-y1 = (m1 - 3/2)*l_span + 3/2 
-plt.plot(l_span, y1, color = "#FF0000")
-
+"""
 m2 = -1.5
 #y2 = (m2 - 3/2)*l_span + 3/2 
 y2 = np.copy(l_span)
-plt.plot(l_span, y2, color = "#e68cde")
+plt.plot(l_span, y2, color = "red")
+"""
+
+"""
+y2 = np.sqrt(l_span**2 + 2*l_span -2)
+plt.plot(l_span, y2, color = "green")
+
+y3 = -np.sqrt(l_span**2 + 2*l_span -2)
+plt.plot(l_span, y3, color = "green")
+"""
+
+"""
+y2 = 2*l_span - 3/2
+plt.plot(l_span, y2, color = "yellow")
+"""
+
+"""
+m3 = 31/15
+b3 = -(941/600)
+y3 = m3*l_span + b3
+plt.plot(l_span, y3, color = "#FFD333")
+"""
 
 
 plt.axis([l_start, l_end, h_start, h_end])
