de55930bdee621ebf80228345c248a3138407123
MacGregor <magwin2012@gmail.com>
2021-11-29 18:53:40 -0500

    Made start and endpoints an input

--- Py_Code/par_driver.py
+++ Py_Code/par_driver.py
@@ -16,30 +16,32 @@ eps = .1
 d = 2
 data = []
 
-steps = 1000
+n_steps = 100
 
-l_start = .5
-l_end = 1.5
-l_step = .01
-l_span = np.arange(l_start, l_end+l_step, l_step)
+l_start = float(input("x start: "))
+l_end = float(input("x end: "))
+
+h_start = float(input("y start: "))
+h_end = float(input("y end: "))
+
+step_sz = float(input("Step size: "))
+
+l_span = np.arange(l_start, l_end+step_sz, step_sz)
 len_l = len(l_span)
 
-h_start = 1
-h_end = 2
-h_step = .0001
-h_span = np.arange(h_start, h_end+h_step, h_step)
+
+h_span = np.arange(h_start, h_end+step_sz, step_sz)
 len_h = len(h_span)
 
 
-
-many_traj = np.empty(shape = (len_l, len_h, steps+1, 2), dtype = np.float16)
+many_traj = np.empty(shape = (len_l, len_h, n_steps+1, 2), dtype = np.float16)
 
 
 start_time = TVA.tic()
 for l in range(len_l):
     print("Starting " + str(l) + "/" + str(len_l) + " l's.")
     many_traj[l] = Parallel(n_jobs = -1, batch_size = 3)(
-        delayed(fxns.calc_trajectory)(l_span[l], H, steps) for H in h_span)
+        delayed(fxns.calc_trajectory)(l_span[l], H, n_steps) for H in h_span)
 time_elapsed = TVA.toc(start_time)
 print(time_elapsed)
 
@@ -53,14 +55,13 @@ file = open((np_folder + folder_name + "notes.txt"), "w+")
 file.write("Time run: " + folder_name[:-1] + "\n")
 file.write(time_elapsed + "\n")
 file.write("====================\n")
-file.write("Number of steps: " + str(steps) + "\n\n")
+file.write("Number of n_steps: " + str(n_steps) + "\n")
+file.write("Step size: " + str(step_sz) + "\n\n")
 
 file.write("l_start: " + str(l_start) + "\n")
 file.write("l_end: " + str(l_end) + "\n")
-file.write("l_step: " + str(l_step) + "\n\n\n")
 
 
 file.write("h_start: " + str(h_start) + "\n")
 file.write("h_end: " + str(h_end) + "\n")
-file.write("h_step: " + str(h_step) + "\n")
 file.close()
